ff1237a79bc7a6741556e293eca6b3eeac00e745
Javier Arboleda <reivaj49@gmail.com>
2018-11-27 13:45:01 -0500

    BS4 frontend with carousel

--- public/scripts/calculator.js
+++ public/scripts/calculator.js
@@ -5,7 +5,11 @@ $("#fib_button").click(function(){
   fibUrl  = baseUrl + "fibonacci/" + $("#fib_number").val();;
   console.log(fibUrl);
   $.get(fibUrl, function(data, status){
-      $("#fib_result").val(`${data}`);
+      if (data == "") {
+        $("#fib_result").text("0");
+      } else {
+        $("#fib_result").html(`${data}`);
+      }
   })
 });
 
@@ -13,16 +17,18 @@ $("#fact_button").click(function(){
   factUrl = baseUrl + "factorial/" +$("#fact_number").val();
   console.log(factUrl);
   $.get(factUrl, function(data, status){
-      $("#fact_result").val(`${data}`);
+    if (data == "") {
+      $("#fact_result").text("0");
+    } else {
+      $("#fact_result").html(`${data}`);
+    }
   })
 });
 
 $("#arit_button").click(function(){
-  console.log();
-
   aritUrl = baseUrl + "arithmetic?operation=" + $("#arit_operation").val() + "&n1=" + $("#arit_num1").val() + "&n2=" + $("#arit_num2").val();
   console.log(aritUrl);
   $.get(aritUrl, function(data, status){
-      $("#arit_result").val(`${data}`);
+      $("#arit_result").html(`${data}`);
   })
 });
