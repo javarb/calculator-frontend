182e403fb77246526320b9c8cc1081621bc10337
Javier Arboleda <reivaj49@gmail.com>
2018-12-18 11:40:03 -0500

    Apply sugesstions in PR #2 #3

--- public/scripts/calculator.js
+++ public/scripts/calculator.js
@@ -1,20 +1,18 @@
 baseUrl ="http://localhost:8080/api/";
 
 $("#fib_button").click(function(){
-  fibUrl  = baseUrl + "fibonacci/" + $("#fib_number").val();;
-  console.log(fibUrl);
+  fibUrl  = baseUrl + "fibonacci/" + $("#fib_number").val();
   $.get(fibUrl, function(data, status){
       if (data == "") {
         $("#fib_result").text("0");
       } else {
-        $("#fib_result").html(`${data}`);
+        $("#fib_result").html(`${data}`);       
       }
   })
 });
 
 $("#fact_button").click(function(){
   factUrl = baseUrl + "factorial/" +$("#fact_number").val();
-  console.log(factUrl);
   $.get(factUrl, function(data, status){
     if (data == "") {
       $("#fact_result").text("0");
@@ -25,8 +23,7 @@ $("#fact_button").click(function(){
 });
 
 $("#arit_button").click(function(){
-  aritUrl = baseUrl + "arithmetic?operation=" + $("#arit_operation").val() + "&n1=" + $("#arit_num1").val() + "&n2=" + $("#arit_num2").val();
-  console.log(aritUrl);
+  aritUrl = baseUrl + "arithmetic?operation=" + $("#arit_operation").val() + "&n1=" + $("#arit_num1").val() + "&n2=" + $("#arit_num2").val();  
   $.get(aritUrl, function(data, status){
       $("#arit_result").html(`${data}`);
   })
